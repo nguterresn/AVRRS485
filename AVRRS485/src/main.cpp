6153ca18c61ef97ea6943e2070cf0d5573d5f3f9
Nuno Nogueira <nunonogueira@MacBook-Pro-de-Nuno.local>
2019-11-20 12:50:13 +0000

    cpp

--- AVRRS485/src/main.cpp
+++ AVRRS485/src/main.cpp
@@ -5,7 +5,6 @@
 #define SLAVE2_ADDR 14
 
 uint8_t ADDR;
-//
 
 #define LED_PIN   13
 #define ADDR3_PIN 11
@@ -16,8 +15,20 @@ uint8_t ADDR;
 #define BUT2_PIN  6
 #define WREN_PIN  2
 
-void asynch9_init(long BAUD) {
-  // put your code here, to setup asynchronous serial communication using 9 bits:
+#define FOSC 8000000 // Clock Speed #define BAUD 9600
+#define BAUD 9600
+#define MYUBRR FOSC/16/BAUD-1
+
+void asynch9_init(long ubrr) {
+
+  /*Set baud rate */
+  UBRR0H = (unsigned char)(ubrr>>8); 
+  UBRR0L = (unsigned char) ubrr;
+  /* Enable receiver and transmitter */ 
+  UCSR0B = (1<<RXEN0)|(1<<TXEN0);
+
+  /* Set frame format: 8data, 2stop bit */ 
+  UCSR0C = (1<<USBS0)|(3<<UCSZ00);
 }
 
 void send_addr(uint8_t addr) {
@@ -25,7 +36,15 @@ void send_addr(uint8_t addr) {
 }
 
 void send_data(uint8_t data) {
-  // put your code here, to send a data byte:
+    // put your code here, to send a data byte:
+    /* Wait for empty transmit buffer */
+    while (!(UCSRnA & (1<<UDREn))));
+    /* Copy 9th bit to TXB8 */
+    UCSRnB &= ~(1<<TXB8); 
+    if (data & 0x0100) // 10101010 & 100000000
+      UCSRnB |= (1<<TXB8);
+    /* Put data into buffer, sends the data */ 
+    UDRn = data;
 }
 
 uint8_t get_data(uint8_t *data) {
@@ -34,6 +53,8 @@ uint8_t get_data(uint8_t *data) {
 
 void setup() {
   // put your setup code here, to run once:
+  asynch9_init(MYUBRR);
+  pinMode(LED_PIN, OUTPUT);
 }
 
 void loop() {
